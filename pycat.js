000e5fb18f467c30a9dd0183be10729c0281d05f
Matthew Viego <mjv35@pitt.edu>
2019-02-04 17:35:24 -0500

    added data lists capability

--- pycat.js
+++ pycat.js
@@ -3,6 +3,83 @@ PyCat Routines
 Michael Bigrigg
 */
 
+/* getDataLists()
+
+   The following function is repsonisble for returning an array of lists consisting of strings that meet certain criteria (being a phone number, url, etc).
+   The function relies heavily on regular expressions to determine if a given string belongs in any of the lists.
+   A for loop will go through every single entry in wordlistsymbols resulting in an O(n) runtime.
+
+*/
+
+function getDataLists(wordlistsymbols){
+  var hashtagslist = [];
+  var mentionslist = [];
+  var phonenumberslist = [];
+  var urlslist = [];
+  var emailslist = [];
+  var ipaddresslist = [];
+  var dateslist = [];
+  var timeslist = [];
+  var currencylist = [];
+  for (i=0;i<wordlistsymbols.length;i++) {
+    var phonenumber = /^(\([0-9]{3}\)\s*|[0-9]{3}\-)[0-9]{3}-[0-9]{4}$/;
+    var url = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
+    var email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var ipaddress = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    var date = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+    var time = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/;
+    var currency = /^\$?[0-9]+(\.[0-9][0-9])?$/;
+    if(wordlistsymbols[i].charAt(0) == '#'){
+      hashtagslist.push(wordlistsymbols[i]);
+    }
+    else if(wordlistsymbols[i].charAt(0) == '@'){
+      mentionslist.push(wordlistsymbols[i]);
+    }
+    else if(wordlistsymbols[i].match(phonenumber)){
+      phonenumberslist.push(wordlistsymbols[i]);
+    }
+    else if(wordlistsymbols[i].match(url)){
+      urlslist.push(wordlistsymbols[i]);
+    }
+    else if(wordlistsymbols[i].match(email)){
+      emailslist.push(wordlistsymbols[i]);
+    }
+    else if(wordlistsymbols[i].match(ipaddress)){
+      ipaddresslist.push(wordlistsymbols[i]);
+    }
+    else if(wordlistsymbols[i].match(date)){
+      var parts = wordlistsymbols[i].split("/");
+      var day = parseInt(parts[1], 10);
+      var month = parseInt(parts[0], 10);
+      var year = parseInt(parts[2], 10);
+      var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+      if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)){  //determine leap year
+        monthLength[1] = 29;
+      }
+      if (day > 0 && day <= monthLength[month - 1] && year > 0 && month > 0 && month <= 12){
+        dateslist.push(wordlistsymbols[i]);
+      }
+    }
+    else if(wordlistsymbols[i].match(time)){
+      timeslist.push(wordlistsymbols[i]);
+    }
+    else if(wordlistsymbols[i].match(currency)){
+      currencylist.push(wordlistsymbols[i]);
+    }
+  }
+  return [hashtagslist, mentionslist, phonenumberslist, urlslist, emailslist, ipaddresslist, dateslist, timeslist, currencylist];
+}
+
+function createDataListDisplay(list, head){
+  for(i=0;i<list.length;i++){
+    head += list[i] + ", ";
+  }
+  if(list.length == 0){
+    head += "None";
+  }
+  return head;
+}
+
 function prepare(intext)
 {
 <!-- replace the carriage return with a blank space -->
